0000034d99c43ac6827d0fd495030289770b41e1
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2022-12-22 21:28:07 -0500

    1671762486:make push 04-00023793

--- make-legit.sh
+++ make-legit.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-#if ! hash cargo 2>/dev/null; then
-#sudo rm -rf $HOME/.cargo/bin
-#sudo curl https://sh.rustup.rs -sSf > rustup.sh && chmod +x rustup.sh && ./rustup.sh -y
-#source $HOME/.cargo/env
-#fi
+if ! hash cargo 2>/dev/null; then
+sudo -su $(whoami) rm -rf $HOME/.cargo/bin
+sudo -su $(whoami) curl https://sh.rustup.rs -sSf > rustup.sh && chmod +x rustup.sh && ./rustup.sh -y
+source $HOME/.cargo/env
+fi
 
 #ENV VARS
 OS=$(uname)
