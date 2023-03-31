3de79350a700d67d4f5a6b124d5dccfd22a39f27
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-03-31 14:59:30 -0400

    .github/workflows/rust-cross.yml: act.mk

--- act.mk
+++ act.mk
@@ -1,2 +1,7 @@
-ubuntu-18:
-	@act -vbr  -W .github/workflows/rust-cross.yml -P ubuntu-latest=rust:1.67
+
+act-rust-1_67:## 	docker rust image
+	@act -vr  -W .github/workflows/rust-cross.yml -P ubuntu-latest=rust:1.67
+act-rust-1_68:## 	docker rust image
+	@act -vr  -W .github/workflows/rust-cross.yml -P ubuntu-latest=rust:1.68
+act-rust-1_69:## 	docker rust image
+	@act -vr  -W .github/workflows/rust-cross.yml -P ubuntu-latest=rust:1.69
