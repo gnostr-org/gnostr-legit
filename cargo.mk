ba4e2362edc0b4c69b9a32ee4266b94b0a32eb00
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-04-03 20:39:16 -0400

    cargo.mk:

--- cargo.mk
+++ cargo.mk
@@ -1,20 +1,25 @@
 cargo-build:## 	cargo-build
+	@echo "export PATH=$(CARGO_PATH)/bin:$(PATH)"
 	@. $(HOME)/.cargo/env
 	@cargo b
 cargo-install:## 	cargo-install
+	@echo "export PATH=$(CARGO_PATH)/bin:$(PATH)"
 	@. $(HOME)/.cargo/env
 	@cargo install --path $(PWD)
-	@echo "export PATH=$(CARGO_PATH)/bin:$(PATH)"
 cargo-build-release:## 	cargo-build-release
+	@echo "export PATH=$(CARGO_PATH)/bin:$(PATH)"
 	@. $(HOME)/.cargo/env
 	@cargo b --release
 cargo-check:## 	cargo-check
+	@echo "export PATH=$(CARGO_PATH)/bin:$(PATH)"
 	@. $(HOME)/.cargo/env
 	@cargo c
 cargo-bench:## 	cargo-bench
+	@echo "export PATH=$(CARGO_PATH)/bin:$(PATH)"
 	@. $(HOME)/.cargo/env
 	@cargo bench
 cargo-test:## 	cargo-test
+	@echo "export PATH=$(CARGO_PATH)/bin:$(PATH)"
 	@. $(HOME)/.cargo/env
 	@cargo test
 # vim: set noexpandtab:
