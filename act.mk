11708e63b601d3311fe1ed5b5f1ed72ec7cafa0c
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-12-16 13:49:50 -0500

    GNUmakefile:act.mk:cargo.mk:docker.mk:update make config

--- act.mk
+++ act.mk
@@ -1,9 +1,9 @@
 1:=cargo-check
 2:=cargo-build
-act-help:### 	
+act-help:### 	act-help
 	@awk 'BEGIN {FS = ":.*?####"} /^[a-zA-Z_-]+:.*?####/ {printf "\033[36m%-15s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
 .PHONY:github-workspace cargo-build cargo-check
-automate:####	run act -vr
+automate:#### 	run act -vr
 	@echo $@ $1 $2 $3
 	@export $(cat ~/GH_TOKEN.txt) && act $(JOB) -v $(BIND) $(REUSE) \
 	-W .github/workflows/$@.yml
