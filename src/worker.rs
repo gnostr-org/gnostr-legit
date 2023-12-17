194e056b2102c59b3819d282e1784418c8d426e7
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-12-17 00:20:59 -0500

    src/worker.rs:apply cargo clippy

--- src/worker.rs
+++ src/worker.rs
@@ -1,94 +1,105 @@
-use std::sync::mpsc;
 use crypto::digest::Digest;
 use crypto::sha1;
+use std::sync::mpsc;
 use time;
 
 pub struct Worker {
-    id:      u32,
-    digest:  sha1::Sha1,
-    tx:      mpsc::Sender<(u32, String, String)>,
-    target:  String,
-    tree:    String,
-    parent:  String,
-    author:  String,
-    message: String,
-    timestamp: time::Tm,
-    weeble: String,
-    wobble: String,
-    blockheight: String
+	id: u32,
+	digest: sha1::Sha1,
+	tx: mpsc::Sender<(u32, String, String)>,
+	target: String,
+	tree: String,
+	parent: String,
+	author: String,
+	message: String,
+	timestamp: time::Tm,
+	weeble: String,
+	wobble: String,
+	blockheight: String,
 }
 
 impl Worker {
-    pub fn new(id:          u32,
-               target:      String,
-               tree:        String,
-               parent:      String,
-               author:      String,
-               message:     String,
-               timestamp:   time::Tm,
-               weeble:      String,
-               wobble:      String,
-               blockheight: String,
-               tx:        mpsc::Sender<(u32, String, String)>) -> Worker {
-        Worker {
-            id:        id,
-            digest:    sha1::Sha1::new(),
-            tx:        tx,
-            target:    target,
-            tree:      tree,
-            parent:    parent,
-            author:    author,
-            message:   message,
-            timestamp: timestamp,
-            weeble:    weeble,
-            wobble:    wobble,
-            blockheight: blockheight
-        }
-    }
+	pub fn new(
+		id: u32,
+		target: String,
+		tree: String,
+		parent: String,
+		author: String,
+		message: String,
+		timestamp: time::Tm,
+		weeble: String,
+		wobble: String,
+		blockheight: String,
+		tx: mpsc::Sender<(u32, String, String)>,
+	) -> Worker {
+		Worker {
+			id: id,
+			digest: sha1::Sha1::new(),
+			tx: tx,
+			target: target,
+			tree: tree,
+			parent: parent,
+			author: author,
+			message: message,
+			timestamp: timestamp,
+			weeble: weeble,
+			wobble: wobble,
+			blockheight: blockheight,
+		}
+	}
 
-    pub fn work(&mut self) {
-        let tstamp = format!("{}", self.timestamp.strftime("%s %z").unwrap());
+	pub fn work(&mut self) {
+		let tstamp =
+			format!("{}", self.timestamp.strftime("%s %z").unwrap());
 
-        let mut value  = 0u32;
-        loop {
-            let (raw, blob) = self.generate_blob(value, &tstamp);
-            let result = self.calculate(&blob);
+		let mut value = 0u32;
+		loop {
+			let (raw, blob) = self.generate_blob(value, &tstamp);
+			let result = self.calculate(&blob);
 
-            if result.starts_with(&self.target) {
-                self.tx.send((self.id, raw, result));
-                break;
-            }
+			if result.starts_with(&self.target) {
+				self.tx.send((self.id, raw, result));
+				break;
+			}
 
-            value += 1;
-        }
-    }
+			value += 1;
+		}
+	}
 
-    fn generate_blob(&mut self, value: u32, tstamp: &str) -> (String, String) {
-        let raw = format!("tree {}\n\
+	fn generate_blob(
+		&mut self,
+		value: u32,
+		tstamp: &str,
+	) -> (String, String) {
+		let raw = format!(
+			"tree {}\n\
                            parent {}\n\
                            author {} {}\n\
                            committer {} {}\n\n\
                           {:04}/{:06}/{:06}/{:02}/{:08x}/{}",
-                          self.tree,
-                          self.parent,
-                          self.author, tstamp,
-                          self.author, tstamp,
-                          self.weeble.trim(),
-                          self.wobble.trim(),
-                          self.blockheight.trim(),
-                          self.id,
-                          value,
-                          self.message);
-        //be careful when changing - fails silently when wrong.
-        let blob = format!("commit {}\0{}", raw.len(), raw);
+			self.tree,
+			self.parent,
+			self.author,
+			tstamp,
+			self.author,
+			tstamp,
+			self.weeble.trim(),
+			self.wobble.trim(),
+			self.blockheight.trim(),
+			self.id,
+			value,
+			self.message
+		);
+		//be careful when changing - fails silently when wrong.
+		let blob = format!("commit {}\0{}", raw.len(), raw);
 
-        (raw, blob)
-    }
+		(raw, blob)
+	}
 
-    fn calculate(&mut self, blob: &str) -> String {
-        self.digest.reset();
-        self.digest.input_str(blob);
+	fn calculate(&mut self, blob: &str) -> String {
+		self.digest.reset();
+		self.digest.input_str(blob);
 
-        self.digest.result_str()
-    }
+		self.digest.result_str()
+	}
 }
