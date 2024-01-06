74f7267d59fb584c45b3ef25d20b5b4a05109df6
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-01-06 17:04:35 -0500

    src/main.rs:temp

--- src/main.rs
+++ src/main.rs
@@ -399,8 +399,9 @@ fn main() -> io::Result<()> {
 	println!("&gnostr_sec before pad: {:?}", &gnostr_sec);
 	println!("&gnostr_sec after pad: {:?}", &gnostr_sec.pad(64, '0', Alignment::Right, true));
 
-	let s = "12345".pad(64, '0', Alignment::Right, true);
+	let s = "".pad(64, '0', Alignment::Right, true);
 	println!("s: {:?}", s);
+
 	// echo "000000b64a065760e5441bf47f0571cb690b28fc" | openssl dgst -sha256 | sed 's/SHA2-256(stdin)= //g'
 	//
 	//
