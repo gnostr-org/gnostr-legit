0000086dc9d25350814d76dd5fec4fe6b927d298
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-02 13:33:50 -0500

    "tree":"e4b4bf4ef24854c34493cf462f676215c6e6f779","parent":"00000aba8440b1329522f212fc788ffd1660706e","weeble":"2052","blockheight":"832856","wobble":"383917","bit":"01","nonce":"00007946","message":"use gnostr_bins:mine nip commit data"

--- src/worker.rs
+++ src/worker.rs
@@ -86,6 +86,7 @@ impl Worker {
 			parent {}\n\
 			author {} {}\n\
 			committer {} {}\n\n\
+            \n\n
             \n\"tree\":\"{}\",\"parent\":\"{}\",\"weeble\":\"{:04}\",\"blockheight\":\"{:06}\",\"wobble\":\"{:}\",\"bit\":\"{:02}\",\"nonce\":\"{:08x}\",\"message\":\"{:}\"",
 			self.tree,
 			self.parent,
@@ -99,7 +100,7 @@ impl Worker {
 			self.id, value,
 			self.message
 		);
-        //print!("raw={}\n",raw);
+        print!("raw={}\n",raw);
 
 		//be careful when changing - fails silently when wrong.
 		let blob = format!("commit {}\0{}", raw.len(), raw);
