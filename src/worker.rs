000000031a7d2b68a506b2ce61a4a612dbf0acc1
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-01-10 18:46:16 -0500

    gnostr-legit-test
    06:0002d420

--- src/worker.rs
+++ src/worker.rs
@@ -59,7 +59,7 @@ impl Worker {
                            parent {}\n\
                            author {} {}\n\
                            committer {} {}\n\n\
-						   {}\n{:02}:{:08x}",
+						   {}\n\n{:02}:{:08x}",
                           self.tree,
                           self.parent,
                           self.author, tstamp,
